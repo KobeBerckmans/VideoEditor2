4e52cf2da499db678aba96f5e4b591b9bddc9985
KobeBerckmans <kobe.berckmans@student.ehb.be>
2024-10-31 14:27:48 +0100

    Initial Commit

---
+++ VideoEditor/VideoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditorApp.swift
+//  VideoEditor
+//
+//  Created by Kobe Berckmans on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
